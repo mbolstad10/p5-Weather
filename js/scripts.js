05b5b65e1705d2c93069c91696c9cd02429d0908
mbolstad10 <mdbolstad@hotmail.com>
2014-12-09 16:19:55 -0800

    base 2

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,92 @@ $.simpleWeather({
     }
   });
 });
+
+var icons = { 
+      "clear-day" : "B", 
+      "clear-night" : "C", 
+      "rain" : "R", 
+      "snow" : "G", 
+      "sleet" : "X", 
+      "wind" : "S", 
+      "fog" :"N", 
+      "cloudy" : "Y",
+      "partly-cloudy-day" : "H", 
+      "partly-cloudy-night" : "I"
+    };
+ 
+    // Cities
+    var cities = {
+      "Seattle" : {coords: {latitude: 47.6097, longitude:122.3331}},
+      "Spokane" : {coords: {latitude: 47.6589, longitude: 117.4250 }},
+      "Cheney" : {coords: {latitude:47.4886, longitude: 117.5786}},
+      
+      };
+
+
+      // Get Weather  
+    function loadWeather(cityCoords){
+      
+      console.log(cityCoords);
+ 
+      var latlng = cityCoords.coords.latitude + "," + cityCoords.coords.longitude;
+              
+      var forecastURL = "https://api.forecast.io/forecast/e79ce4f49850f7741774f156ee9198bc/" + latlng;
+ 
+      $.ajax({
+          url: forecastURL,
+          jsonpCallback: 'jsonCallback',
+          contentType: "application/json",
+          dataType: 'jsonp',
+          success: function(json) {
+ 
+            // Returned Data 
+            console.log(json);
+ 
+            // Display Data Dynamically
+            $("#current_temp").html(Math.round(json.currently.temperature)+"&#176;F");
+            $("#current_summary").html(json.currently.summary);
+            $("#current_temp").attr("data-icon",icons[json.currently.icon]);
+ 
+          },
+          error: function(e) {
+             console.log(e.message);
+          }
+      });
+ 
+    }
+    
+    function loadCity(city){
+      $("#location").html(city);
+
+      if (city.toLowerCase() == "current location") {
+        if ( navigator.geolocation )
+          navigator.geolocation.getCurrentPosition(loadWeather,loadDefaultCity);
+        else {
+          loadDefaultCity();
+        }
+
+      } else {
+        loadWeather(cities[city.toLowerCase()]);
+      }
+
+    }
+
+    function loadDefaultCity(){
+      loadCity("Cheney");
+    }
+    
+ 
+    // On Page Load
+    $(document).ready(function(){
+ 
+      // Load City, then load weather
+      loadCity("Cheney");
+ 
+      // Bind click to City
+ 
+      $(".item").bind("click",function(){
+        loadCity($(this).html());
+      });
+ 
+    });
